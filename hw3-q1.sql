74e580d7a721feeb47958d97dfa48ed14fbc60d3
yangyuxue1994 <yangyuxue2333@gmail.com>
2020-01-26 11:41:04 -0800

    update q1

--- hw3-q1.sql
+++ hw3-q1.sql
@@ -1,30 +1,38 @@
---hw3-q1
+-- hw3-q1
+-- author: AJ
+-- revise: CY
 
---- All distinct combination pairs of origin_city & dest_city
-WITH t1 AS (
-	SELECT DISTINCT f0.origin_city, f0.dest_city
-FROM FLIGHTS AS f0
-WHERE f0.canceled!=1)
+WITH VAR1 AS (
+SELECT *
+FROM (
+    SELECT DISTINCT F1.origin_city,F1.dest_city
+    FROM Flights AS F1
+    WHERE NOT EXISTS (
+        SELECT *
+        FROM Flights as F2
+        WHERE F2.dest_city = F1.origin_city
+            AND F1.dest_city = F2.origin_city
+    )) AS T),
+VAR2 AS (
+    SELECT DISTINCT F1.origin_city,F1.dest_city
+    FROM Flights AS F1, Flights AS F2
+    WHERE F1.origin_city = F2.dest_city 
+        AND F1.dest_city = F2.origin_city
+        AND F1.origin_city < F1.dest_city
+)
+SELECT COUNT(*) AS num_connected_cities
+FROM (
+SELECT *
+FROM VAR1
+UNION
+SELECT *
+FROM VAR2 ) u
 
---- All duplicates combinations (Seattle-Boston and Bosto-Seattle)
-,t2 AS (SELECT DISTINCT f1.origin_city, f1.dest_city
-FROM FLIGHTS AS f1
-WHERE f1.canceled!=1
-EXCEPT 
-SELECT DISTINCT f2.dest_city, f2.origin_city
-FROM FLIGHTS AS f2
-WHERE f2.canceled!=1)
+-- Rows Returned: 1
+-- Query Execution Time: 00:00:44.599
+-- Output Columns:
 
---- Duplicates/2+Unique Combination=Unique Combination
-SELECT
-(SELECT COUNT(*)/2 FROM (SELECT * FROM t1 EXCEPT SELECT * FROM t2) tmp) +
-(SELECT COUNT(*) FROM t2) AS num_connected_cities;
-
-
---- number of rows: 1
---- how long the query took: 55s
---- output:
 
 -- num_connected_cities
+-- ---------------
 -- 2351
-
