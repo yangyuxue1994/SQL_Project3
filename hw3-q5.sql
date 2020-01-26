8c144c95a734e648a85f33de5eaa3efb7c30349e
Yuxue Yang <chery@uw.edu>
2020-01-26 12:47:47 -0800

    Update column name hw3-q5.sql

--- hw3-q5.sql
+++ hw3-q5.sql
@@ -12,31 +12,35 @@ VAL1 AS (
     FROM Flights AS F1
     WHERE F1.origin_city = 'Seattle WA'
 )
-SELECT DISTINCT V.dest_city
+SELECT DISTINCT V.dest_city AS city
 FROM VAL AS V, VAL1 AS V1
 WHERE V1.dest_city = V.origin_city
     AND V.dest_city != 'Seattle WA'
 
 -- Rows Returned: 256
 -- Query Execution Time: 00:00:22.156
--- Output Columns:
--- Dothan AL
--- Toledo OH
--- Peoria IL
--- Yuma AZ
--- Bakersfield CA
--- Daytona Beach FL
--- Laramie WY
--- North Bend/Coos Bay OR
--- Erie PA
--- Guam TT
--- Columbus GA
--- Wichita Falls TX
--- Hartford CT
--- Myrtle Beach SC
--- Arcata/Eureka CA
--- Kotzebue AK
--- Medford OR
--- Providence RI
--- Green Bay WI
--- Santa Maria CA
+-- Output Columns: (first 20 rows)
+
+-- city        
+-- ------------
+-- Aguadilla PR
+-- Akron OH    
+-- Bangor ME   
+-- Birmingham A
+-- Bozeman MT  
+-- Buffalo NY  
+-- Burlington V
+-- Charleston S
+-- Charlotte Am
+-- Charlottesvi
+-- Columbia SC 
+-- Columbus OH 
+-- Dayton OH   
+-- Fayetteville
+-- Fort Myers F
+-- Grand Rapids
+-- Greensboro/H
+-- Greer SC    
+-- Hyannis MA  
+-- Jacksonville
+
