ca567d635aa4ca2f38213591cf6ce5e7d0d6d253
Yuxue Yang <chery@uw.edu>
2020-01-26 11:51:00 -0800

    Update output column hw3-q2.sql

--- hw3-q2.sql
+++ hw3-q2.sql
@@ -26,24 +26,27 @@ ORDER BY V1.actual_time, V1.origin_city
 
 -- Rows Returned: 339
 -- Query Execution Time: 00:00:16.584
--- Output Columns:
--- Bend/Redmond OR	Los Angeles CA	10
--- Burbank CA	New York NY	10
--- Las Vegas NV	Chicago IL	10
--- New York NY	Nashville TN	10
--- Newark NJ	Detroit MI	10
--- Sacramento CA	Atlanta GA	10
--- Washington DC	Minneapolis MN	10
--- Boise ID	Chicago IL	11
--- Boston MA	Philadelphia PA	11
--- Buffalo NY	Orlando FL	11
--- Cincinnati OH	New Haven CT	11
--- Denver CO	Honolulu HI	11
--- Denver CO	Orlando FL	11
--- Denver CO	Philadelphia PA	11
--- Fort Myers FL	Chicago IL	11
--- Houston TX	Salt Lake City UT	11
--- Minneapolis MN	Newark NJ	11
--- Pittsburgh PA	Dallas/Fort Worth TX	11
--- Indianapolis IN	Houston TX	12
--- Phoenix AZ	Dallas/Fort Worth TX	12
+-- Output Columns: (20 rows)
+
+-- origin_city      dest_city       time      
+-- ---------------  --------------  ----------
+-- Bend/Redmond OR  Los Angeles CA  10        
+-- Burbank CA       New York NY     10        
+-- Las Vegas NV     Chicago IL      10        
+-- New York NY      Nashville TN    10        
+-- Newark NJ        Detroit MI      10        
+-- Sacramento CA    Atlanta GA      10        
+-- Washington DC    Minneapolis MN  10        
+-- Boise ID         Chicago IL      11        
+-- Boston MA        Philadelphia P  11        
+-- Buffalo NY       Orlando FL      11        
+-- Cincinnati OH    New Haven CT    11        
+-- Denver CO        Honolulu HI     11        
+-- Denver CO        Orlando FL      11        
+-- Denver CO        Philadelphia P  11        
+-- Fort Myers FL    Chicago IL      11        
+-- Houston TX       Salt Lake City  11        
+-- Minneapolis MN   Newark NJ       11        
+-- Pittsburgh PA    Dallas/Fort Wo  11        
+-- Indianapolis IN  Houston TX      12        
+-- Phoenix AZ       Dallas/Fort Wo  12 
